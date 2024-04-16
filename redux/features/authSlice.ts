616c3e89d8d32f04cc9ff1a87232298bd848f0c9
Puskar-Roy <puskarroy600@gmail.com>
2024-04-16 22:26:12 +0530

    feat : redux toolkit sync

--- redux/features/authSlice.ts
+++ redux/features/authSlice.ts
@@ -1,35 +1,36 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { InitialAuthState } from "@/interfaces";
 
-const initialState: InitialAuthState = {
-  value: {
-    isAuth: false,
-    userName: "",
-    uuid: "",
-    isAdmin: false,
-  },
-};
-
+const storedTasks = localStorage.getItem("tasks");
+const initialState: InitialAuthState = storedTasks
+  ? JSON.parse(storedTasks)
+  : {
+      value: {
+        isAuth: false,
+        userName: "",
+        uuid: "",
+        isAdmin: false,
+      },
+    };
 export const auth = createSlice({
   name: "auth",
   initialState: initialState,
-  reducers:{
-    logout:()=>{
-        return initialState
+  reducers: {
+    logout: () => {
+      return initialState;
     },
-    login:(state,action:PayloadAction<string>)=>{
-        return{
-            value:{
-                isAuth:true,
-                userName:action.payload,
-                uuid:'98+6+5659+9+8+9',
-                isAdmin:false
-            }
-        }
-    }
-  }
+    login: (state, action: PayloadAction<string>) => {
+      return {
+        value: {
+          isAuth: true,
+          userName: action.payload,
+          uuid: "98+6+5659+9+8+9",
+          isAdmin: false,
+        },
+      };
+    },
+  },
 });
 
-
-export const { login , logout } = auth.actions;
+export const { login, logout } = auth.actions;
 export default auth.reducer;
